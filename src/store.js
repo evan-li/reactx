85ef168cb4c444c240c9d25f9fe733993442efe2
evan <574583177@qq.com>
2018-02-01 11:37:04 +0800

    修改

--- src/store.js
+++ src/store.js
@@ -13,16 +13,45 @@ let _state = {};
  */
 let _modulesState = {};
 
+let _mutations = {};
+let _actions = {};
+
+/**
+ * 状态管理器
+ *
+ * @see state 状态树, 聚集所有的状态
+ * @see getter 从state中派生出的状态
+ * @see mutation 状态修改, 修改状态的操作只能在 mutation 中进行, 且不可以是异步的
+ * @see action 逻辑操作, 可以调用一系列的 mutation 进行状态修改, 也可以执行异步操作
+ * @see module 模块, 模块中可以继续包含子模块
+ *
+ * @see options {state, getters, mutations, actions, modules} 初始化或合并状态的选项
+ *
+ * @constructor ()
+ *
+ * @property state object 根状态
+ * @property getters 从state中派生出的状态
+ *
+ * @method commit(type, payload, options) 提交 mutation
+ * @method dispatch(type, payload) 提交 action
+ * @method combineStore(options) 合并状态管理器, 选项参见 options, 相当于连续调用了 combineState/combineMutations/combineGetters/combineActions/combineModules
+ * @method combineState(state) 合并状态
+ * @method combineMutations(mutations) 合并 mutation
+ * @method combineGetters(getters) 合并getters
+ * @method combineActions(actions) 合并actions
+ * @method combineModules(modules) 合并模块, 每个模块所包含的都是一个 options
+ */
 class Store {
     /**
      * Store的构造方法
      */
     constructor(){
         this.state = {};
-        this.mutations = {};
         this.getters = {};
-        this.actions = {};
-        this.modules = {};
+        // mutations与actions不开放, modules要不要开放待定
+        // this.mutations = {};
+        // this.actions = {};
+        // this.modules = {};
 
         // todo 插件
     }
@@ -58,11 +87,48 @@ class Store {
      */
     combineStore(options){
         options = options || {};
-        this.combineModules(options.modules || null);
         this.combineState(options.state || null);
         this.combineMutations(options.mutations || null);
         this.combineGetters(options.mutations || null);
         this.combineActions(options.actions || null);
+        this.combineModules(options.modules || null);
+    }
+
+    /**
+     * 合并 state
+     * @param state
+     */
+    combineState(state) {
+        if(!state) return ;
+        deepMerge(_state, state);
+        this.state = deepCopy(_state)
+    }
+
+    /**
+     * 合并 getters
+     * @param getters
+     */
+    combineGetters(getters) {
+        if(!getters) return ;
+        deepMerge(this.getters, getters)
+    }
+
+    /**
+     * 合并 mutations
+     * @param mutations
+     */
+    combineMutations(mutations) {
+        if(!mutations) return ;
+        deepMerge(this.mutations, mutations)
+    }
+
+    /**
+     * 合并 actions
+     * @param actions
+     */
+    combineActions(actions){
+        if(!actions) return ;
+        deepMerge(this.actions, actions)
     }
 
     /**
@@ -79,42 +145,6 @@ class Store {
         // todo 合并模块中的 actions
     }
 
-    /**
-     * 合并 state
-     * @param state
-     */
-    combineState(state) {
-        if(!state) return ;
-        deepMerge(_state, state);
-        this.state = deepCopy(_state)
-    }
-
-    /**
-     * 合并 mutations
-     * @param mutations
-     */
-    combineMutations(mutations) {
-        if(!mutations) return ;
-        deepMerge(this.mutations, mutations)
-    }
-
-    /**
-     * 合并 getters
-     * @param getters
-     */
-    combineGetters(getters) {
-        if(!getters) return ;
-        deepMerge(this.getters, getters)
-    }
-
-    /**
-     * 合并 actions
-     * @param actions
-     */
-    combineActions(actions){
-        if(!actions) return ;
-        deepMerge(this.actions, actions)
-    }
 }
 
 export default Store;
